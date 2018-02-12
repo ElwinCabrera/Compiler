8892536dc2cb06f9df06d74b3080da220e25a137
Elwin <elwincab@buffalo.edu>
2018-02-12 18:25:19 +0000

    Formatting and moved things around but code was unchanged

--- scanner.c
+++ scanner.c
@@ -13,37 +13,14 @@ static int column = 1;
 static int end_row = 1;
 static int end_column = 1;
 
-void update_location()
-{
-    row = end_row;           
-    column = end_column;     
-                    
-    if(end_row != yylineno)  
-    {                        
-        end_column = 1;      
-        end_row = yylineno;  
-    }                        
-    else                     
-    {                        
-        end_column += yyleng;
-    }                        
-}
-
-int handle_token(int token)
-{
-    if(token > 0)
-    {
-        printf("%d %s %d %d\n", token, yytext, row, column);
-    }
-
-    return token;
-}
+void update_location(void);
+int handle_token(int token);
 
 int main(int argc, char* argv[])
 {
     if(argc < 1)
     {
-        printf("Specify an input file.");
+        printf("Specify an input file.\n");
         return 1;
     }
 
@@ -51,7 +28,7 @@ int main(int argc, char* argv[])
 
     if(!input_file)
     {
-        printf("The input file was not found.");
+        printf("The input file was not found.\n");
         return 1;
     }
 
@@ -67,4 +44,32 @@ int main(int argc, char* argv[])
     fclose(input_file);
 
     return 0;
+}
+
+
+
+int handle_token(int token)
+{
+    if(token > 0)
+    {
+        printf("%d %s %d %d\n", token, yytext, row, column);
+    }
+
+    return token;
+}
+
+void update_location(void)
+{
+    row = end_row;           
+    column = end_column;     
+                    
+    if(end_row != yylineno)  
+    {                        
+        end_column = 1;      
+        end_row = yylineno;  
+    }                        
+    else                     
+    {                        
+        end_column += yyleng;
+    }                        
 }
