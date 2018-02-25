6f22e7eeab92e925d9d3b2d18aef13fa13f3e53e
Mark <mgordon52192@gmail.com>
2018-02-25 08:04:56 -0500

    Merge conflicts

--- runner.c
+++ runner.c
@@ -1,43 +1,19 @@
 #include <stdio.h>
 
-extern int yyleng;
-extern int yylineno;
-extern char* yytext;
-
-extern int yylex();
+//The followuing are defined in a diferent module, why the extern keyword is used.
+extern int yyleng; /* Will give us the length to a token value */
+extern int yylineno; /* Gives us more information (the line number) when we're parsing to generate a helpful error message later.*/  
+extern char* yytext; /* Gives us the actual value of the tokens we're parsing*/
+extern int yylex(); /* Will help us parse our file into tokens (retuns 0 if done)*/
 extern void yyset_in(FILE*);
 
+void update_location(void); /* keeps track of the current row and coulumn we're at in our file*/
+int handle_token(int token); /* Used to output tokens as we are parsing them in a specific format (format is from project description)*/
 
 static int row = 1;
 static int column = 1;
 static int end_row = 1;
 static int end_column = 1;
-
-void update_location()
-{
-    row = end_row;           
-    column = end_column;     
-                    
-    if(end_row != yylineno)  
-    {                        
-        end_column = 1;      
-        end_row = yylineno;  
-    }                        
-    else                     
-    {                        
-        end_column += yyleng;
-    }                        
-}
-
-int handle_token(int token)
-{
-    if(token > 0)
-    {
-        printf("%d %s %d %d\n", token, yytext, row, column);
-    }
-
-    return token;
-}
 
 int main(int argc, char* argv[])
 {
@@ -67,4 +43,30 @@ int main(int argc, char* argv[])
     // fclose(input_file);
 
     return 0;
+}
+
+void update_location()
+{
+    row = end_row;           
+    column = end_column;     
+                    
+    if(end_row != yylineno)  
+    {                        
+        end_column = 1;      
+        end_row = yylineno;  
+    }                        
+    else                     
+    {                        
+        end_column += yyleng;
+    }                        
+}
+
+int handle_token(int token)
+{
+    if(token > 0)
+    {
+        printf("%d %s %d %d\n", token, yytext, row, column);
+    }
+
+    return token;
 }
