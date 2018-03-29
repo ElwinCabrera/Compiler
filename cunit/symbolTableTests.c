3474433427cdf4a1f64f88a3f472e80e61342eca
Mark <mgordon52192@gmail.com>
2018-03-29 00:43:56 -0400

    More tests

--- cunit/symbolTableTests.c
+++ cunit/symbolTableTests.c
@@ -20,6 +20,31 @@ void test01() {
   CU_ASSERT_PTR_NULL(find_in_scope(s, "testFunction"));
 }
 
+// Add an entry to a parent scope
+// Add a child to that scope
+// The entry should be findable from the child scope
+void test02() { 
+
+  struct scope * s = new_scope(NULL);
+  struct scope * s2 = new_scope(s);
+
+  struct symtab * expected = add_entry(s, 0, "testFunction", NULL);
+
+  CU_ASSERT_PTR_EQUAL(expected, find_in_scope(s2, "testFunction"));
+}
+
+// Create a scope
+// Add a child to that scope
+// Add entry to child scope
+// The entry should NOT be findable from the parent scope
+void test03() {
+  struct scope * s = new_scope(NULL);
+  struct scope * s2 = new_scope(s);
+
+  struct symtab * expected = add_entry(s2, 0, "testFunction", NULL);
+
+  CU_ASSERT_PTR_NULL(find_in_scope(s, "testFunction"));
+}
 
 int addSymbolTableTests() {
     CU_pSuite Suite = CU_add_suite("Symbol Table Tests", NULL, NULL);
@@ -30,6 +55,8 @@ int addSymbolTableTests() {
   if ( (    
       NULL == CU_add_test(Suite, "test00", test00) 
       || NULL == CU_add_test(Suite, "test01", test01) 
+      || NULL == CU_add_test(Suite, "test02", test02) 
+      || NULL == CU_add_test(Suite, "test03", test03) 
 	) ) {
     return 0;
   }
