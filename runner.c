7510f9c0a56534ebbed5853f22ceadeebb3faa62
Elwin <elwincab@buffalo.edu>
2018-03-29 14:02:05 +0000

    maybe better?

--- runner.c
+++ runner.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
-// Defined by yacc, this function handles the calls to yylex
 extern int yyparse();
+extern void yyset_in(FILE *);
 
+FILE *inputFile;
 int main(int argc, char* argv[])
-{    
-    printf("yyparse exit code: %d\n", yyparse());
-    return 0;
+{
+   //printf("yyparse exit code: %d\n", yyparse());
+ if (argc == 1){
+     printf("No program to compile\n");
+     return 1;
+ }
+ else{
+     const char* program = argv[1];
+     inputFile = fopen(program, "r");
+     yyset_in(inputFile);
+     yyparse();
+     return 0;
+ }
+   return 0;
 }
