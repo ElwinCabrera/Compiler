a21b0c5bd6a92e56dc46c72d31899a124a1c0742
Mark <mgordon52192@gmail.com>
2018-03-29 00:38:09 -0400

    Add two basic tests

--- cunit/symbolTableTests.c
+++ cunit/symbolTableTests.c
@@ -1,7 +1,24 @@
 #include "symbolTableTests.h"
 #include "../symbolTable.h"
 
-void test00() { }
+// Add an entry to a scope and try to find it
+void test00() { 
+
+  struct scope * s = new_scope(NULL);
+
+  struct symtab * expected = add_entry(s, 0, "testFunction", NULL);
+
+  CU_ASSERT_PTR_EQUAL(expected, find_in_scope(s, "testFunction"));
+}
+
+// Look for an entry that doesn't exist
+// Should be NULL
+void test01() { 
+
+  struct scope * s = new_scope(NULL);
+
+  CU_ASSERT_PTR_NULL(find_in_scope(s, "testFunction"));
+}
 
 
 int addSymbolTableTests() {
@@ -12,6 +29,7 @@ int addSymbolTableTests() {
   }
   if ( (    
       NULL == CU_add_test(Suite, "test00", test00) 
+      || NULL == CU_add_test(Suite, "test01", test01) 
 	) ) {
     return 0;
   }
