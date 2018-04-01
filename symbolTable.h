01e8e5c14412d8d04a51890d26a2f970aabb9aa3
Mark <mgordon52192@gmail.com>
2018-04-01 03:10:19 -0400

    Remove old function declaration

--- symbolTable.h
+++ symbolTable.h
@@ -34,6 +34,5 @@ SYMTAB *find_entry(SYMTAB *, char*);
 SYMTAB *add_entry(SCOPE *, struct type *, char*, char*);
 void print_symbol(SYMTAB *);
 void print_symbol_table(SCOPE *);
-void print_symbol_table_impl(SCOPE *);
 
 #endif
