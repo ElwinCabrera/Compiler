5a5fa8a6a99aa393f88ce4297f045c979d54d2af
Mark <mgordon52192@gmail.com>
2018-04-01 01:59:53 -0400

    Missed the actual type files

---
+++ types.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <string.h>
+#include "types.h"
+#include "y.tab.h"
+
+SYMTYPE * add_type(SYMTYPE* start, int type, char* name) {
+
+    if(!name || find_type(start, name)) {
+        return NULL;
+    }
+
+    SYMTYPE * new = malloc(sizeof(SYMTYPE));
+    
+    new->subtype = type;
+
+    switch(type) {
+        case FUNCTION:
+            new->details.record = malloc(sizeof(struct function_details));
+            break;
+        case RECORD:
+            new->details.record = malloc(sizeof(struct record_details));
+            break;
+        case ARRAY:
+            new->details.array = malloc(sizeof(struct array_details));
+            break;
+        default:
+            new->details.primitive = NULL;
+            break;
+    }
+    new->name = name;
+    new->next = start;
+
+    return new;
+}
+
+SYMTYPE * find_type(SYMTYPE* start, char* name) {
+
+    if(!name) {
+        return NULL;
+    }
+
+    SYMTYPE * s = start;
+
+    while(s) {
+        if(strcmp(name, s->name) == 0) {
+            return s;
+        }
+        s = s->next;
+    }
+
+    return NULL;
+}
---
+++ types.h
@@ -0,0 +1,37 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include "symbolTable.h"
+
+struct function_details {
+    struct scope * parameters;
+    struct type * return_type;
+};
+
+struct array_details {
+    int dimensions;
+    struct type * element_type;
+};
+
+struct record_details {
+    struct scope * members;
+};
+
+typedef struct type {
+    int subtype;
+    char * name;
+    union {
+        struct function_details * function;
+        struct array_details * array;
+        struct record_details * record;
+        void * primitive;
+    } details;
+    struct type * next;
+} SYMTYPE;
+
+SYMTYPE * type_list;
+
+SYMTYPE * add_type(SYMTYPE *, int, char*);
+SYMTYPE * find_type(SYMTYPE *, char*);
+
+#endif
