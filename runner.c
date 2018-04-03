d6ee13405afee5a00c921c70a11d262a59853dd7
dht7166 <hdtran@buffalo.edu>
2018-04-02 20:12:54 -0400

    fix some silly mistake

--- runner.c
+++ runner.c
@@ -39,7 +39,7 @@ int main(int argc, char* argv[])
                 //Print symbol table
             }
             else{
-                program = argv[i]
+                program = argv[i];
                 read_program = true;
             }
         }
