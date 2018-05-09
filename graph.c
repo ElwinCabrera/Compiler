2696c3e63e7215e8c52a5b798aa8db863d3edd50
Hoan duc tran <hdtran@buffalo.edu>
2018-05-09 22:05:44 +0000

    graph.c edited online with Bitbucket

--- graph.c
+++ graph.c
@@ -10,14 +10,14 @@
 #include "intermediate_code.h"
 
 
-VALNUM* newNode(TAC_OP OP,int number, TAC *instruction, VALNUM *left, VALNUM *right, ADDRESS *value){
+VALNUM* newNode(TAC_OP OP,int number, TAC *code, VALNUM *left, VALNUM *right, ADDRESS *value){
 	VALNUM *node = malloc(sizeof(VALNUM));
 	node->op = OP;
 	node->number = number;
-	node->instruction = instruction;
+	node->code = code;
 	node->left = left;
 	node->right = right;
-	node->value = value;
+	node->result = value;
 	return node;
 }
 
@@ -45,9 +45,7 @@ void add_to_graph(GRAPH *gr,VALNUM *node){
 		gr->current = node;
 	}
 	else{
-		printf("ADDING TO GRAPH ERROR FUNCTION\n");
 		gr->head = ll_insertback(gr->head,node);
-		printf("ADDING TO GRAPH ERROR FUNCTION2\n");
 		gr->current = node;
 	}
 }
@@ -56,13 +54,59 @@ void process_tac(GRAPH *gr, TAC *code){
 	// process the three address code
 	//first step is to analyze the three address code
 	TAC_OP OP = code->op;
-	VALNUM *node = newNode(OP,0,code,NULL,NULL,NULL);
-	if(gr->head!=NULL){
-		node->number = gr->current->number + 1;	
+	ADDRESS *left = code->x;
+	ADDRESS *right = code->y;
+	ADDRESS *result  = code->result;
+	//need to check if left and right are already in the graph
+	VALNUM *ll = find_node_with_address(gr,left);
+	if (ll == NULL){
+		// create a new ll node
+		ll = newNode(I_VALNUM_NODE,gr->current->number + 1,NULL,NULL,NULL,left);
+		add_to_graph(gr,ll);
 	}
-	printf("HERE STUCK\n");
-	add_to_graph(gr,node);
-	printf("DONE PROCESS TAC INNER\n");
+	VALNUM *rr = find_node_with_address(gr,right);
+	if (rr == NULL && right !=NULL){
+		// create a new ll node
+		rr = newNode(I_VALNUM_NODE,gr->current->number + 1,NULL,NULL,NULL,right);
+		add_to_graph(gr,rr);
+	}
+	VALNUM *old_node = find_node_with_expression(gr,OP,ll,rr);
+	if(old_node==NULL || old_node->number < ll->number || old_node->number < rr->number){
+		// need to make a new node for this expression
+		VALNUM *node = newNode(OP,gr->current->number+1,code,ll,rr,result);
+		add_to_graph(gr,node);	
+	}
+	else{
+		//node with that expression already existed
+		//just need to assign to that node
+		VALNUM *node = newNode(I_VALNUM_LINKED,gr->current->number+1,code,old_node,NULL,result);
+		add_to_graph(gr,node);
+	}
+	
+}
+
+void optimize(GRAPH *gr){
+	// optimization, changing all the code
+	LINKED_LIST *head = gr->head;
+	while(head){
+		//change the tac
+		VALNUM *node = head->value;
+		if(node->code!=NULL){
+			if(node->op == I_VALNUM_LINKED){
+				TAC *code = node->code;
+				code->op = I_ASSIGN;
+				code->y = NULL;
+				code->x = node->left->result;		
+			}
+			else{
+				TAC *code = node->code;
+				code->y = node->left->result;
+				code->x = node->right->result;
+			}
+		}	
+
+	}
+
 }
 
 VALNUM *find_node_with_address(GRAPH *gr,ADDRESS *add){
@@ -70,7 +114,7 @@ VALNUM *find_node_with_address(GRAPH *gr,ADDRESS *add){
 	LINKED_LIST *head = gr->head;
 	while(head!=NULL){
 		VALNUM *node = head->value;
-		ADDRESS *cur_add = node->value;
+		ADDRESS *cur_add = node->result;
 		if(cur_add == add){
 			result = head->value;
 		} 
